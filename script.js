66611c3c9d1255e9c0128e607ad866349aa68288
akshay muthyala <98809257+akshay4away@users.noreply.github.com>
2023-01-09 19:32:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let count = 0;
+
+function incrementCounter(){
+    count++;
+    document.getElementById("counter").innerHTML = count;
+}
+
+function resetCounter(){
+    count=0;
+    text = "";
+    document. getElementById("counter").innerHTML = count;
+    document.getElementById("text").innetHTML = text;
+}
